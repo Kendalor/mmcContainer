7df04940c53f0a0c0b2567f3bdfe6fc35c97282f
Patrick <prehn@students.uni-mainz.de>
2019-07-09 20:45:07 +0200

    testing

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -6,7 +6,8 @@ mkdir -p "$SERVER_VOL"
 
 
 # Start Server
+echo "Dockerfile Params: $PORT, $VANILLA_VERSION, $PACK_VERSION, $PACK_URL"
 echo "eula=true" > /opt/Server/eula.txt
 echo "Entry Point Working"
 chmod +x /opt/Server/ServerStart.sh
-exec /opt/Server/ServerStart.sh
+# exec /opt/Server/ServerStart.sh
