814892c86b5fd061515d8e1862ee80ac9338176a
Patrick Rehn <patrickr@inhouse.nterra.com>
2019-07-10 15:10:17 +0200

    fixed settings-locale

--- ftbContainer/files/docker-entrypoint.sh
+++ ftbContainer/files/docker-entrypoint.sh
@@ -45,9 +45,8 @@ echo "Checking for Settings Locale"
 sleep 1
 if [ ! -f $SERVER_VOL/settings-local.sh ]; then
         echo "export JAVACMD='${JAVACMD}' 
-    export MAX_RAM='${MAX_RAM}'
-    export JAVA_PARAMETERS='${JAVA_PARAMETERS}'
-    " > $SERVER_VOL/settings-local.sh
+export MAX_RAM='${MAX_RAM}'
+export JAVA_PARAMETERS='${JAVA_PARAMETERS}'" > $SERVER_VOL/settings-local.sh
     chmod +x $SERVER_VOL/settings-local.sh
 fi
 
